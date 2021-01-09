d2b5437d1e60d8f215c52990a247047d00762156
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-01-08 16:36:12 -0800

    Remove Button

--- src/Tours.js
+++ src/Tours.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = ({ tours }) => {
+const Tours = ({ tours,removeTour }) => {
   return <section>
     <div className="title">
       <h2>Our Tours</h2>
@@ -8,7 +8,7 @@ const Tours = ({ tours }) => {
     </div>
     <div>
       {tours.map((tour) => {
-        return <Tour key={tour.id} image={tour.image} info={tour.info} price={tour.price} name={tour.name} ></Tour>
+        return <Tour key={tour.id} image={tour.image} info={tour.info} price={tour.price} name={tour.name} removeTour={removeTour}></Tour>
       })}
     </div>
   
